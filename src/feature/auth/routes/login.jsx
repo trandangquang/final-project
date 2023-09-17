e4329cb596fa06609df07cf7c5ac0222d989ff5c
trandangquang <trandangquang2203@gmail.com>
2023-09-17 19:12:31 +0700

    finished login & register feature

--- src/feature/auth/routes/login.jsx
+++ src/feature/auth/routes/login.jsx
@@ -1,5 +1,27 @@
+import { auth } from '@/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LoginForm } from '../components';
 
 export const Login = () => {
-  return <LoginForm />;
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onLogin = async values => {
+    setIsSubmitting(true);
+    await signInWithEmailAndPassword(auth, values.email, values.password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate('/');
+      })
+      .catch(error => {
+        const errorCode = error.errorCode;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+        setIsSubmitting(false);
+      });
+  };
+  return <LoginForm onFinish={onLogin} isSubmitting={isSubmitting} />;
 };
--- src/feature/auth/routes/register.jsx
+++ src/feature/auth/routes/register.jsx
@@ -1,5 +1,30 @@
-import  { RegisterForm } from '../components'
+import { auth } from '@/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RegisterForm } from '../components';
 
 export const Register = () => {
-  return < RegisterForm />
-}
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onRegister = async values => {
+    setIsSubmitting(true);
+    await createUserWithEmailAndPassword(auth, values.email, values.password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        console.log(user);
+
+        navigate('/');
+      })
+      .catch(error => {
+        const errorCode = error.errorCode;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+
+        setIsSubmitting(false);
+      });
+  };
+
+  return <RegisterForm onFinish={onRegister} isSubmitting={isSubmitting} />;
+};
